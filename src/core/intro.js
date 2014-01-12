4787ef5a1e9d0ceac9638b41b5c6c5d4986b5753
zoetrope <a.ikezoe@gmail.com>
2014-01-12 23:49:07 +0900

    change version number

--- src/core/intro.js
+++ src/core/intro.js
@@ -1,5 +1,5 @@
 /**
- * @license ReactiveProperty for AngularJS v0.1.1
+ * @license ReactiveProperty for AngularJS v0.1.2
  * Copyright (c) 2014 zoetrope https://github.com/zoetrope/reactiveproperty-angular/
  * License: MIT
  */
